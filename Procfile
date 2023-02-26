aa972f8aefb7f10613190aa6b9b11d5451a0397e
Jyotirmay Barman <jyotirmay@developerzilla.com>
2023-02-26 17:28:26 +0530

    add procfile & initialized eb cli

---
+++ Procfile
@@ -0,0 +1,3 @@
+clean_install: npm ci
+build: npm run build
+web: npm run start:prod
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    const envs = process.env;
+    return {
+      message: "Hello world",
+      envs
+    };
   }
 }
